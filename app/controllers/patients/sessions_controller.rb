ee8bab6f8e706a3887ca5040ffe7d9f49cf70c5b
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-08-10 13:18:08 +0100

    moved sessions controller to here under patients folder

---
+++ app/controllers/patients/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Patients::SessionsController < ApplicationController
+  layout "empty"
+
+  def new
+  end
+
+  def create
+    @patient = Patient.find_by(email: params[:session][:email].downcase)
+    if @patient && @patient.authenticate(params[:session][:password])
+      patient_sign_in(@patient)
+      redirect_to patient_dashboard_path
+    else
+      flash[:error] = 'Invalid email/password combination'
+      redirect_to patient_signin_path
+    end
+  end
+
+  def destroy
+    patient_sign_out
+    redirect_to patient_signin_path
+  end
+
+end
