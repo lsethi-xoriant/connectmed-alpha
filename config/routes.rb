2db7f441e4b614fffe61f63222095d78619780d8
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-09-05 06:43:16 +0100

    added finish view routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ConnectMed::Application.routes.draw do
     get '/signout', to: 'sessions#destroy', as: 'signout' #get rather than delete bc of issue with twitter bootstrap link_to
     resources :sessions, only: [:new, :create, :destroy]
     resources :consults
-    get '/consult/finish', to: "consults#finish", as: 'consult_finish'
+    get '/consults/finish', to: "consults#finish", as: 'consults_finish'
     resources :pharmacies
   end
   resources :patients
