f6c74666bdd37f221b2a0a981d1749bf9deb188d
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-10-15 18:14:28 +0100

    missed one api key change

--- app/controllers/doctor/consults_controller.rb
+++ app/controllers/doctor/consults_controller.rb
@@ -39,7 +39,7 @@ class Doctor::ConsultsController < ApplicationController
   def show
     @consult = Consult.find(params[:id])
     current_doctor.consults << @consult
-    @opentok = OpenTok::OpenTok.new("45334072","49403b241d18e0d4d3c4da37d89e3577d25a83e0")
+    @opentok = OpenTok::OpenTok.new("45376712","1306cff952b849c1278422a388436d9cd8440e77")
     @tok_token = @opentok.generate_token @consult.sessionId
   end
 
