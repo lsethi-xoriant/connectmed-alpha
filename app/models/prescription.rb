c894e4a282d17406a59e0e550bdf49abd731ddd4
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-02-12 13:11:08 +0000

    fixed bug in prescriptions index where callback function came after it was actually called

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,6 +1,6 @@
 class Prescription < ActiveRecord::Base
-  belongs_to :consults
+  belongs_to :consult
   has_attached_file :image
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"], styles: { medium: "300x300>", thumb: "100x100>" }
 
 end
