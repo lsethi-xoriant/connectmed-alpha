fac94fd42ddfcd0d65edd56bbe7881bd609303cd
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-03-04 03:55:43 +0000

    removed production config changed to see if can solve heorku crash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,19 +20,19 @@ ConnectMed::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Mailer configuration using Gmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@connectmed.co.za'}
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.default_options = {from: 'no-reply@connectmed.co.za'}
+#   config.action_mailer.delivery_method = :smtp
+#   # SMTP settings for gmail
+#   config.action_mailer.smtp_settings = {
+#    :address              => "smtp.gmail.com",
+#    :port                 => 587,
+#    :user_name            => ENV['gmail_username'],
+#    :password             => ENV['gmail_password'],
+#    :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
