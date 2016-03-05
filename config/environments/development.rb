7bbddeb93ed06085ee157a5b5aa551e4f9f85d81
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-03-05 07:55:45 +0000

    changed back to test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ ConnectMed::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@connectmed.co.za'}
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
   config.action_mailer.smtp_settings = {
     :user_name  => ENV['sendgrind_username'],
