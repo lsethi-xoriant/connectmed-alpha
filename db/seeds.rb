97f4a5f300c64c95183a13a8496e958f491d9718
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-03-06 02:02:36 +0000

    changed seeds so can test on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
   sunday = Date.today+1
   time = Time.new(sunday.year,sunday.month,sunday.day, 13, 0);
 
-  for i in 0..3
+  for i in 0..1
     Slot.create(time: time+i*60*30, date:sunday, hour: (time+i*60*30).hour, minute: (time+i*60*30).min,is_open:true)
   end
