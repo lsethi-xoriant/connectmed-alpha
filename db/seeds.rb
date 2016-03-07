5cf40f42d5d4a52fd5f9e55ab40a41a5ee0f66f9
melissa-mccoy <melissa.l.mccoy@gmail.com>
2016-03-07 08:48:17 +0000

    add back prescription view for demo; changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,12 @@
   sunday = Date.today+1
   time = Time.new(sunday.year,sunday.month,sunday.day, 13, 0);
 
-  for i in 0..1
+  for i in 0..3
     Slot.create(time: time+i*60*30, date:sunday, hour: (time+i*60*30).hour, minute: (time+i*60*30).min,is_open:true)
+  end
+
+  monday = Date.today+3
+  time2 = Time.new(monday.year,monday.month,monday.day, 9, 0);
+  for i in 0..3
+    Slot.create(time: time2+i*60*30, date:monday, hour: (time2+i*60*30).hour, minute: (time2+i*60*30).min,is_open:true)
   end
